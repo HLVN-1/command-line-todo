bac092a637de71a40b03f6c95778e80235da6e93
Jason Newsom <jnewsom405@gmail.com>
2024-05-01 18:42:25 -0500

    learned about 'await', unit testing, integration testing, E2E testing, and worked on CLI ToDo List app

--- index.js
+++ index.js
@@ -1,15 +1,50 @@
-const prompts = require("prompts");
+import prompts from "prompts";
+
+let toDoList = [];
 
 const main = async () => {
-  const nextToDo = async () => {
+  const toDoMenu = async () => {
+    return await prompts({
+      type: "select",
+      name: "value",
+      message: "What action to perform?",
+      choices: [
+        { title: "Show ToDo list", value: "S" },
+        { title: "Add to ToDo list", value: "A" },
+        { title: "Delete A ToDo list item", value: "D" },
+        { title: "Quit ToDo list", value: "Q" },
+      ],
+    });
+  };
+  let addToDo = async () => {
     return await prompts({
       type: "text",
       name: "value",
-      message: "What is your next ToDo item?",
+      message: "What is your new ToDo item?",
     });
   };
-  const message = await nextToDo();
-  console.log(message);
+  // let message = await nextToDo();
+  // console.log(message);
+
+  const showToDo = () => {
+    console.log("===================");
+        toDoList.forEach( (x) => console.log(x.value));
+    console.log("===================");
+  };
+
+  let option = {};
+  do{
+    option = await toDoMenu();
+    switch (option.value){
+      case 'S':
+        showToDo();
+        break;
+      case 'A': 
+        toDoList.push(await addToDo());
+        break;
+    }
+  } while(option.value != 'Q');
+  
 };
 
 main().catch(console.error);
